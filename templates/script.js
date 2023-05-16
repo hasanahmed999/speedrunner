23c5270ad60cf21b22c131b152c67d7437dc6b3c
Hasan Ahmed <hasanahmed999@gmail.com>
2023-05-16 15:25:05 -0400

    Add movement logic

--- templates/script.js
+++ templates/script.js
@@ -1,9 +1,53 @@
 /// https://www.codewizardshq.com/javascript-games/
-var canvasWidth = 600;
-var canvasHeight = 400;
+var canvasWidth = 300;
+var canvasHeight = 600;
+var playerWidth = 100;
+var playerHeight = 100;
+
+var player;
+var playerXPosition = canvasWidth/2 - playerWidth/2;
+
+var interval = setInterval(updateCanvas, 20);
+
+function createPlayer(width, height, y) {
+	this.width = width;
+	this.height = height;
+	this.y = y;
+	this.x = playerXPosition;
+
+	
+	this.draw = function() {
+		ctx = gameCanvas.context;
+		ctx.fillStyle = "green";
+		window.addEventListener("keydown", (e) => {
+			if (e.key == 'ArrowRight') {
+				this.x = playerXPosition + playerWidth;
+			}
+			else if (e.key == 'ArrowLeft') {
+				this.x = playerXPosition - playerWidth;
+			}	
+		});
+		window.addEventListener("keyup", (e) => {
+			if (e.key == 'ArrowLeft' || e.key == 'ArrowRight') {
+				this.x = playerXPosition;
+			}
+		});
+		ctx.fillRect(this.x, this.y, this.width, this.height);
+	}
+
+
+
+}
 
 function startGame() {
 	gameCanvas.start();
+	player  = new createPlayer(playerWidth, playerHeight, canvasHeight - playerHeight);
+}
+
+function updateCanvas() {
+	ctx = gameCanvas.context;
+	ctx.clearRect(0, 0, canvasWidth, canvasHeight);
+	player.draw()
 }
 
 var gameCanvas = {
