d2cf08ea2f6f6a2505553928df384fa2252efa4a
Hasan Ahmed <hasanahmed999@gmail.com>
2023-06-26 10:31:57 -0400

    Adding preliminary collision block

--- templates/script.js
+++ templates/script.js
@@ -41,13 +41,15 @@ function createPlayer(width, height, y) {
 
 function startGame() {
 	gameCanvas.start();
-	player  = new createPlayer(playerWidth, playerHeight, canvasHeight - playerHeight);
+	player = new createPlayer(playerWidth, playerHeight, canvasHeight - playerHeight);
+	block = new createBlock(playerWidth, playerHeight);
 }
 
 function updateCanvas() {
 	ctx = gameCanvas.context;
 	ctx.clearRect(0, 0, canvasWidth, canvasHeight);
-	player.draw()
+	player.draw();
+	block.draw();
 }
 
 var gameCanvas = {
@@ -59,3 +61,30 @@ var gameCanvas = {
 		document.body.insertBefore(this.canvas, document.body.childNodes[0]);
 	}
 }
+
+var block;
+function randomNumber() {
+	// between 1 and 2
+	return Math.floor(Math.random() * 2) + 1;
+}
+
+function createBlock(width, height) {
+	// randomly create 1 or 2 blocks. Need a placement parameter to randomly place on left or right or middle
+	num_blocks_created = randomNumber();
+	for (let i = 0; i < num_blocks_created; i++) {
+		// create block. if 2 blocks, make sure we dont create an extra on the same location
+		this.width = width;
+		this.height = height;
+		this.y = 400;
+		this.x = playerXPosition - playerWidth;
+	}
+
+	this.draw = function() {
+		ctx = gameCanvas.context;
+		ctx.fillStyle = "red";	
+		ctx.fillRect(this.x, this.y, this.width, this.height);
+	}
+	
+}
+
+
